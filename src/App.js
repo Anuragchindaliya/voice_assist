903e50c113d4660621bc1954fd8223ff2b971c01
anuragchindaliya <ak1148383@gmail.com>
2021-05-16 16:56:16 +0530

    added button and news key

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const alanKey ='';
+const alanKey ='7fbea83f46a49d2a30d6dc46c712bcbe2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = ()=>{
 
     useEffect(()=>{
         alanBtn({
             key:alanKey,
+            onCommand:({command,articles})=>{
+                if(command==='newsHeadlines'){
+                    console.log(articles);
+                }
+            }
+
         })
     },[]);
 
     return (
         <div>
-            <h1>Anurag</h1>
+            <h1>Anurag hi hai abi b</h1>
         </div>
     );
 }
